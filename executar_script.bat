bda486c5a3e94875ec7daf88567c732bfe8301c0
Pyetro-ZS <pyetromesquita15@gmail.com>
2024-10-14 20:36:01 -0300

    Update executar_script.bat

--- executar_script.bat
+++ executar_script.bat
@@ -1,3 +1,3 @@
 @echo off
-C:\Python312\python.exe C:\Users\pyeto\Downloads\2SIT.2\Leitor_RFID\script.py
+C:\Python312\python.exe C:\Users\pyeto\Downloads\2SIT.2\Leitor_RFID\script.py #mude o diretório do arquivo, e slave como (.bat).
 pause
